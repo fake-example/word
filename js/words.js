fbeaac5c65b229994b2933536b67b8d4360e8187
fakeexaple <108689209+fakeexaple@users.noreply.github.com>
2022-08-21 19:26:22 +0500

    Add files via upload

---
+++ js/words.js
@@ -0,0 +1,46 @@
+const wordList = [
+    {
+        word: "white",
+        hint: "What colour is snow?"
+    },
+    {
+        word: "paris",
+        hint: "What is the capital of France?"
+    },
+    {
+        word: "boots",
+        hint: "Do people wear boots or sandals in winter?"
+    },
+    {
+        word: "red",
+        hint: "What colour are London’s buses?"
+    },
+    {
+        word: "round",
+        hint: "Is the Earth round or square?"
+    },
+    {
+        word: "russia",
+        hint: "What is the largest country in the world?"
+    },
+    {
+        word: "winter",
+        hint: "What is the coldest season?"
+    },
+    {
+        word: "yellow",
+        hint: "What colour is the sun?"
+    },
+    {
+        word: "small",
+        hint: "What is the opposite of “big”?"
+    },
+    {
+        word: "berlin",
+        hint: "What is the capital of Germany?"
+    },
+    {
+        word: "france",
+        hint: "Which is bigger – France or Spain?"
+    },
+]
